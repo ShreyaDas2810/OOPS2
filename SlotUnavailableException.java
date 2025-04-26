2b85d563f37ba16e87193462cf37dd246a933d69
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:21:16 +0530

    Update SlotUnavailableException.java

--- SlotUnavailableException.java
+++ SlotUnavailableException.java
@@ -1,8 +1,24 @@
 /**
- * Custom Exception (XI) for when no suitable parking slot is available.
+ * Exception thrown when a parking slot is unavailable.
  */
 class SlotUnavailableException extends Exception {
+
+    // Default error message
+    private static final String DEFAULT_MESSAGE = "No available slots for the requested vehicle type.";
+
+    /**
+     * Constructor with custom message.
+     *
+     * @param message Custom error message.
+     */
     public SlotUnavailableException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructor with default message.
+     */
+    public SlotUnavailableException() {
+        super(DEFAULT_MESSAGE);
     }
 }
